297d41ade5b5b01e01f9152821b111dd35751387
Angel-Salazar-M <145618356+Angel-Salazar-M@users.noreply.github.com>
2025-11-26 16:38:36 -0600

    ruta

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       class="bg-gradient-to-br from-black to-[#E51515] grid grid-cols-2 grid-rows-1 place-self-start w-full h-16 mb-10 ">
       <div class="grid grid-cols-3 w-70">
         <div class="grid col-start-1 place-content-center my-auto w-full h-full">
-          <router-link to="/home" class="w-full h-full">Inicio</router-link>
+          <router-link to="/" class="w-full h-full">Inicio</router-link>
         </div>
         <div class="grid col-start-2 place-content-start my-auto w-auto w-80">
           <router-link to="/categorias">Categorias</router-link>
