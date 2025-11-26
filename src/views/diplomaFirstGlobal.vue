69e20858809d5b2c2b3bcb3e779a11ce3b01ec91
Angel-Salazar-M <145618356+Angel-Salazar-M@users.noreply.github.com>
2025-11-26 16:25:00 -0600

    commit con miss sofi

--- src/views/diplomaFirstGlobal.vue
+++ src/views/diplomaFirstGlobal.vue
@@ -5,7 +5,7 @@ import footerCard from '../components/footerCard.vue'
 
 <template>
     <div class="rounded-md border border-black shadow-xl bg-gradient-to-br from-[#5A0707] via-[#EF0A0A] to-[#190101] w-3/4 h-fit p-10 grid place-self-center m-15">
-        <img src="@/assets/evidencia portafolio1_01.jpg" alt="" class="object-cover w-3/4 h-fit mx-auto">
+        <img src="@/assets/evidencia portafolio 1_01.jpg" alt="" class="object-cover w-3/4 h-fit mx-auto">
         
     </div>
     <footerCard/>
