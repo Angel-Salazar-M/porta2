181a8cd99aa8aacb1d10ade5d32acf30e98ce270
Angel-Salazar-M <145618356+Angel-Salazar-M@users.noreply.github.com>
2025-12-01 14:43:53 -0600

    error solucion

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div>
+  <div style="text-align: center; margin-top: 4rem;">
+    <h1>404</h1>
+    <p>Página no encontrada</p>
+    <router-link to="/">Volver al inicio</router-link>
+  </div>
+    </div>
+</template>
